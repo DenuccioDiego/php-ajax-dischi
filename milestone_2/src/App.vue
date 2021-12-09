8856486c5ff0e9d55f18441560754406134cf717
DenuccioDiego <diegodenuccio95@gmail.com>
2021-12-09 17:18:17 +0100

    declaration components

--- milestone_2/src/App.vue
+++ milestone_2/src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div>
+    <header>
+      <SiteHeader/>
+    </header>
+
+    <main>
+      <SongCards/>
+    </main>
+
+    <footer>
+    </footer>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SiteHeader from './components/Header.vue';
+import SongCards from './components/SongCards.vue';
+
 
 export default {
-  name: 'App',
+ 
+  
   components: {
-    HelloWorld
-  }
+    SiteHeader,
+    SongCards,
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+
+
+
+
 </style>
---
+++ milestone_2/src/components/SongCards.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <div>
+       de
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+
+export default {
+
+     data(){
+          return{
+               disks:[],
+               api_url: "../../../milestone_1/db.php",
+          };
+     },
+          
+     
+
+     mounted() {
+     axios
+          .get(this.api_url)
+          .then((r) => {
+               this.disks = r.cards
+          })
+          .catch((e) => {
+               this.error = e;
+          });
+     },
+  
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
