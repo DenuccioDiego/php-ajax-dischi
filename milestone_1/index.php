d0214c120c04f38d1a5f5019704b3526f0083274
DenuccioDiego <diegodenuccio95@gmail.com>
2021-12-09 15:51:26 +0100

    milestone_1 complete

--- milestone_1/index.php
+++ milestone_1/index.php
@@ -12,36 +12,70 @@ require __DIR__. '/db.php';
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Php-Ajax-Dischi</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <style>
+          
+          header>div{
+               background-color: #2e3a46;
+               height:4rem;
+               padding-left:20px;
+               padding-top:5px
+              
+          }
+          header img{
+               
+               width: 50px;
+               
+          }
+          main{
+               background-color: #1e2d3b;
+               min-height: 92.1vh;
+          }
+          .card{
+               
+               background-color: #2e3a46;
+
+          }
+
+     </style>
+
 </head>
+
 <body>
      
 
+     <header>
 
-     
+          <div>
+               <img src="./asset/Spotify_logo_without_text.svg" alt="">
+          </div>
+          
+     </header>
+
+
      <main>
-          <div class="container">
-               <div class="row justify-content-center gy-4">
+          <div class="container pt-5">
+
+               <div class="d-flex flex-wrap justify-content-center gap-5 gx-0">
 
                     <?php foreach ($cards as $card): ?>
-                    <div class="col-2">
-                         <div class="card">
+                    
+                         <div class="card col-2 text-center p-3 border-0 rounded-0 align-items-stretch">
                               <img src="<?php echo $card['poster'] ?>" alt="">
-                              <h3 class="text-white">
+                              <h3 class="text-white pt-3 pb-1">
                                    <?php echo $card['title'] ?>
                               </h3>
                               <p class="text-muted">
                                    <span>
                                         <?php echo $card['author'] ?>
-                                   </span>
+                                   </span><br>
                                    <span>
                                         <?php echo $card['year'] ?>
                                    </span>
                               </p>
 
                          </div>
-                    </div>
+                    
                     <?php endforeach ?> 
-
 
                </div>
                
